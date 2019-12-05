75c666bc25370e7f42075e741b855cbcf57ba01d
Mariola Pasko <paskomariola@gmail.com>
2019-12-05 18:32:58 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,8 +52,7 @@ def runner
   welcome 
   card_total = initial_round
   until card_total > 21 
-  
-  until display_card_total(card_total) > 21
+  card_total = hit?(card_total)
 end_game
 end 
 end 
